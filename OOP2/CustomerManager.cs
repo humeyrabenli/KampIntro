59c89091ba3e17c4e1cd13bd92d6d618096608c0
hmyrbnl <hmyrbnl@gmail.com>
2021-02-25 00:02:22 +0300

    w

---
+++ OOP2/CustomerManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    //business classes
+    class CustomerManager
+    {
+        public void Add(Musteri musteri)
+        {
+            //if (musteri.)
+            //{
+
+            //}
+
+        }
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Ayşe";
+            musteri1.Soyadi = "Yılmaz";
+            musteri1.TcNo = "22222";
+
+
+            //kodlama.io
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54123";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "1234587936";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(musteri1);
+            customerManager.Add(musteri2);
+
+
+                 
+          
+        }
+    }
+}
